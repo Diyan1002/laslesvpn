7f6c896793fb4be8d63a811db4b1d98eb820cb1f
Muhammad Diyan Irfan <diyanirfan56@gmail.com>
2025-11-19 21:43:12 +0500

    nav

--- src/Components/Navbar .jsx
+++ src/Components/Navbar .jsx
@@ -41,8 +41,9 @@ const Navbar = () => {
         <div className="lg:hidden flex items-center" onClick={toggleMenu}>
           <button className="text-[#4F5665] focus:outline-none">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path>
+</svg>
+
           </button>
         </div>
 
